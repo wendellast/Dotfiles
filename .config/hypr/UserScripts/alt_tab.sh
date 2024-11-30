ee0071c7cafd73513742656811ea6c9961311d24
wendellast <wendelalves01a@hotmail.com>
2024-11-30 11:33:07 -0300

    remove scripts

--- .config/hypr/UserScripts/alt_tab.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-windows=$(hyprctl clients -j | jq -r '.[].title')
-selected=$(echo "$windows" | rofi -dmenu -p "Windows:")
-
-if [ -n "$selected" ]; then
-  hyprctl dispatch focuswindow "$selected"
-fi
